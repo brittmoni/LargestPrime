6c3a840d153b1bd4f583a71965fd368a011b1ff9
Brittany F <brittfranklin92@gmail.com>
2020-11-24 11:49:04 -0500

    Addition of pseudocode

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -8,3 +8,16 @@ public class LargestPrime {
         return -1;
     }
 }
+
+/*
+1. Write loop to perform operation multiple times
+2. Divide number by itself
+3. Reduce number by 1
+4. Redivide number new number
+5. Get remainders
+6. If remainder is 0, divide numbers by itself
+    6a. Reduce by 1
+    6b. Determine if number is prime
+7. For the first prime number that comes, return that number
+8. Exit loop
+ */
